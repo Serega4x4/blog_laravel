6a3bd455b1a158f25d58eed83836dff3ec5ddcc9
Serega4x4 <serega1988bykov@gmail.com>
2025-01-22 13:10:28 +0100

    add Frontend

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
+    Route::get('/', 'IndexController')->name('index');
 });
+// Auth::routes();
