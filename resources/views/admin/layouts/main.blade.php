a2b19866bf48c1d06e15e5bc5e85fd8440078b19
Serega4x4 <serega1988bykov@gmail.com>
2025-01-23 11:03:28 +0100

    add strategy of CRUD & add route for categories

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -33,16 +33,8 @@
   </nav>
   <!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-
-    </div>
-    <!-- /.sidebar -->
-  </aside>
-
+@include('admin.includes.sidebar')
 @yield('content')
 
   <footer class="main-footer">
