fbbc1a9c4af83ff6d20c431dd422631df46b99bf
Serega4x4 <serega1988bykov@gmail.com>
2025-01-23 11:53:50 +0100

    add form of Create with route

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -8,7 +8,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
+            <h1 class="m-0">Adding Category</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -26,11 +26,13 @@
       <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
         <div class="row">
-            <div class="col-2">
-                <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Add category</a>
-            </div>
             <div class="col-12">
-                Categories
+                <form action="#" class="w-25">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Name of category">
+                      </div>
+                      <input type="submit" class=" btn btn-primary" value="Add">
+                </form>
             </div>
           <!-- ./col -->
         </div>
--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -8,7 +8,7 @@
 with font-awesome or any other icon font library -->
 <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
     <li class="nav-item">
-    <a href="#" class="nav-link">
+    <a href="{{ route('admin.category.index') }}" class="nav-link">
         <i class="nav-icon fas fa-th-list"></i>
         <p>
         Category
