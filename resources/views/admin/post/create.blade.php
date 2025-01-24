3993d7b93008c45a795d6f38fa244e3072dd35a0
Serega4x4 <serega1988bykov@gmail.com>
2025-01-24 15:12:53 +0100

    add summernote and new Content for that

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -27,15 +27,27 @@
         <!-- Small boxes (Stat box) -->
         <div class="row">
             <div class="col-12">
-                <form action="{{ route('admin.post.store') }}" method="POST" class="w-25">
+                <form action="{{ route('admin.post.store') }}" method="POST">
                     @csrf
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="title" placeholder="Name of post">
+
+                    <div class="form-group" class="w-25">
+                        <input type="text" class="form-control" name="title" placeholder="Name of post"
+                        value="{{ old('title') }}">
                         @error('title')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
                       </div>
-                      <input type="submit" class=" btn btn-primary" value="Add">
+
+                      <div class="form-group">
+                        <textarea id="summernote" name="content">{{ old('content') }}</textarea>
+                        @error('content')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                      </div>
+
+                      <div class="form-group">
+                          <input type="submit" class=" btn btn-primary" value="Add">
+                      </div>
                 </form>
             </div>
           <!-- ./col -->
