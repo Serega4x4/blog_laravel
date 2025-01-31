2fbc7a01637f3854e77c0011a5f9b9da9f85118c
Serega4x4 <serega1988bykov@gmail.com>
2025-01-31 14:45:46 +0100

    add users in admin panel

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -9,6 +9,15 @@ with font-awesome or any other icon font library -->
 <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
     <li class="nav-item">
+        <a href="{{ route('admin.user.index') }}" class="nav-link">
+            <i class="nav-icon fas fa-users"></i>
+            <p>
+            User
+            </p>
+        </a>
+    </li>
+
+    <li class="nav-item">
         <a href="{{ route('admin.post.index') }}" class="nav-link">
             <i class="nav-icon fas fa-clipboard"></i>
             <p>
