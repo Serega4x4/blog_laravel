ead401e2d5cb4d80b189eb104c7f356c527c4952
Serega4x4 <serega1988bykov@gmail.com>
2025-02-07 10:03:00 +0100

    add update, delete, edit in Personal/commets

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,7 +60,13 @@ class User extends Authenticatable implements MustVerifyEmail
         $this->notify(new SendWithQueueNotification());
     }
 
-    public function likedPosts() {
+    public function likedPosts()
+    {
         return $this->belongsToMany(Post::class, 'post_user_likes', 'user_id', 'post_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'user_id', 'id');
     }
 }
